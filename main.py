c03d7679099166ee038e785c6d9ae198b2e232b7
AArcangelo <98560442+AArcangelo@users.noreply.github.com>
2022-01-27 22:49:15 +0000

    added more code

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from movie import Movie
+
+MyMovie = Movie(False,60,"action","Anna Kendrick","PG",1,"Flowers")
+Movie2 = Movie(True,40,"mystery","Tom Hanks","Pg-13",2,"Little Rocks")
+Movie3 = Movie (False,80,"action","Tom Holland","PG",2,"Tom's Fall")
+
+
+MovieList =[MyMovie,Movie2,Movie3]
+print(len(MovieList))
+for movie in MovieList:
+  print(movie.title)
+
+ #def __init__(self,_animated,_length,_genre,_cast,_rating,_part,_title):
---
+++ movie.py
@@ -0,0 +1,26 @@
+class Movie:
+
+  def __init__(self,_animated,_length,_genre,_cast,_rating,_part,_title):
+    self.title = _title
+    self.animated = _animated
+    self.length = _length
+    self.genre = _genre
+    self.cast = _cast
+    self.rating = _rating
+    self.part = _part
+
+
+'''
+animated 
+length
+Genre
+cast/Stars
+rating 
+part 1,2,3
+
+
+
+
+
+
+'''
